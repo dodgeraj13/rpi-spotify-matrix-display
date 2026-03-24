b35a286b71d55da672dc748895a7b02da33967fb
Kyle Johnson <kyjohnson09@gmail.com>
2026-03-24 17:30:45 -0500

    tweak

--- transitions/easing.py
+++ transitions/easing.py
@@ -2,7 +2,7 @@ import math
 
 # Transition timings (frames @ 60fps)
 SLIDE_FRAMES = 36
-BOUNCE_FRAMES = 26
+BOUNCE_FRAMES = 28
 
 def ease_out_back(t: float, s: float = 1.70158) -> float:
     """Overshoots the target before returning to it (rubberband effect)."""
